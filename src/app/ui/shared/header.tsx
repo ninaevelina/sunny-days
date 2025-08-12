f5251519762033fc012750832ac77de9e7add30a
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2025-08-12 15:43:23 +0200

    refactor: wraps the link with logo in a nav

--- src/app/ui/shared/header.tsx
+++ src/app/ui/shared/header.tsx
@@ -5,14 +5,20 @@ export default function Header() {
   return (
     <header className="border-b border-black py-5">
       <div className="w-4/5 mx-auto flex justify-center md:justify-start">
-        <Link href={"/"}>
-          <Image
-            src={"/sunny-days.svg"}
-            alt="Sunny Days Logo"
-            height={100}
-            width={100}
-          />
-        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/"}>
+                <Image
+                  src={"/sunny-days.svg"}
+                  alt="Sunny Days Logo"
+                  height={100}
+                  width={100}
+                />
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
