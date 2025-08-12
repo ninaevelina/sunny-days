5c10413df247f079e7a10d0f8adc18580ad559ab
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2025-08-12 15:37:36 +0200

    feat: adds scaffolding for the header with logo #31

---
+++ src/app/ui/shared/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="border-b border-black py-5">
+      <div className="w-4/5 mx-auto flex justify-center md:justify-start">
+        <Link href={"/"}>
+          <Image
+            src={"/sunny-days.svg"}
+            alt="Sunny Days Logo"
+            height={100}
+            width={100}
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
